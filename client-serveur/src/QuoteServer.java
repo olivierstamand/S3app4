196aac8bf0e68a0adbc82ee1936548b81c4225d8
olivierstamand <113619052+olivierstamand@users.noreply.github.com>
2023-06-24 14:22:12 -0400

    commit bitches

---
+++ client-serveur/src/QuoteServer.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.net.DatagramSocket;
+
+
+public class QuoteServer {
+    public static void main(String[] args) throws IOException {
+        new QuoteServerThread().start();
+    }
+}
+
+
+
